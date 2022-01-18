c0065ae021cc79e909af05ec070b209a27d3c1ce
Tiphaine CHARTRAIN <tiphaine.chartrain@consertotech.pro>
2022-01-18 16:06:17 +0100

    init

---
+++ tests/AppBundle/Security/GithubUserProviderTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\AppBundle\Security;
+
+use AppBundle\Entity\User;
+use AppBundle\Security\GithubUserProvider;
+
+class GithubUserProviderTest extends \PHPUnit_Framework_TestCase
+{
+    private $client;
+    private $serializer;
+    private $streamedResponse;
+    private $response;
+
+    public function setUp()
+    {
+        $this->client = $this->getMockBuilder('GuzzleHttp\Client')
+            ->disableOriginalConstructor()
+            ->setMethods(['get'])
+            ->getMock();
+
+        $this->serializer = $this
+            ->getMockBuilder('JMS\Serializer\Serializer')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->streamedResponse = $this
+            ->getMockBuilder('Psr\Http\Message\StreamInterface')
+            ->getMock();
+
+        $this->response = $this
+            ->getMockBuilder('Psr\Http\Message\ResponseInterface')
+            ->getMock();
+    }
+
+    public function testLoadUserByUsernameReturningAUser()
+    {
+        $this->client
+            ->expects($this->once()) // Nous nous attendons à ce que la méthode get soit appelée une fois
+            ->method('get')
+            ->willReturn($this->response);
+
+        $this->response
+            ->expects($this->once()) // Nous nous attendons à ce que la méthode getBody soit appelée une fois
+            ->method('getBody')
+            ->willReturn($this->streamedResponse);
+
+        $userData = ['login' => 'a login', 'name' => 'user name', 'email' => 'adress@mail.com', 'avatar_url' => 'url to the avatar', 'html_url' => 'url to profile'];
+        $this->serializer
+            ->expects($this->once()) // Nous nous attendons à ce que la méthode deserialize soit appelée une fois
+            ->method('deserialize')
+            ->willReturn($userData);
+
+        $githubUserProvider = new GithubUserProvider($this->client, $this->serializer);
+        $user = $githubUserProvider->loadUserByUsername('an-access-token');
+
+        $expectedUser = new User($userData['login'], $userData['name'], $userData['email'], $userData['avatar_url'], $userData['html_url']);
+
+        $this->assertEquals($expectedUser, $user);
+        $this->assertEquals('AppBundle\Entity\User', get_class($user));
+    }
+
+    public function testLoadUserByUsernameThrowingException()
+    {
+        $this->client
+            ->expects($this->once()) // Nous nous attendons à ce que la méthode get soit appelée une fois
+            ->method('get')
+            ->willReturn($this->response);
+
+        $this->response
+            ->expects($this->once()) // Nous nous attendons à ce que la méthode getBody soit appelée une fois
+            ->method('getBody')
+            ->willReturn($this->streamedResponse);
+
+        $this->serializer
+            ->expects($this->once()) // Nous nous attendons à ce que la méthode deserialize soit appelée une fois
+            ->method('deserialize')
+            ->willReturn([]);
+
+        $this->expectException('LogicException');
+        $this->expectExceptionMessage('Did not managed to get your user info from Github.');
+
+        $githubUserProvider = new GithubUserProvider($this->client, $this->serializer);
+        $user = $githubUserProvider->loadUserByUsername('an-access-token');
+    }
+     public function tearDown()
+    {
+        $this->client = null;
+        $this->serializer = null;
+        $this->streamedResponse = null;
+        $this->response = null;
+    }
+}
